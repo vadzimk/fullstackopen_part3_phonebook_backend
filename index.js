0236b19051055b99c722bd48896eafacc85dc73d
Vadzim <vadzimk@hotmail.com>
2021-01-02 19:07:39 -0800

    completed 3.5 phonebook backend step5

--- index.js
+++ index.js
@@ -38,23 +38,47 @@ app.get('/info',
         )
     })
 
-app.get('/api/persons/:id',(req, res)=>{
+app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
-    const person = persons.find((p)=>p.id===id)
+    const person = persons.find((p) => p.id === id)
 
 
-    if(!person){
+    if (!person) {
         res.status(404).end()
         return  // necessary to exit from the callback
     }
     res.send(person)
 })
 
-app.delete('/api/persons/:id', (req, res)=>{
+app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
-    persons = persons.filter(p=>p.id !== id)
+    persons = persons.filter(p => p.id !== id)
     res.status(204).end()
+})
 
+// const generateId = () => Math.max(...persons.map(p => p.id)) + 1
+const generateRandomId =()=>{
+    let id
+    do {
+        id = Math.floor(Math.random() * (Math.pow(10, 20)-1)+1)
+    }
+    while(persons.find(p=>p.id===id))
+    return id
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    if (!body.name || !body.number) {
+        res.status(400).json({error: "name and number must be specified"})
+        return
+    }
+    const newContact = {
+        name: body.name,
+        number: body.name,
+        id: generateRandomId()
+    }
+    persons = persons.concat(newContact)
+    res.json(newContact) // echos back the created contact
 })
 
 const PORT = 3001
