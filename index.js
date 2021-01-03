07233fc29115cea4dd7d47cf00caf022354f4411
Vadzim <vadzimk@hotmail.com>
2021-01-02 21:29:53 -0800

    completed 3.8 phonebook backend step8

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ let persons = [
 const app = express()
 app.use(express.json())
 
-app.use(morgan('tiny'))
+morgan.token('data', (req, res)=>JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 app.get('/api/persons',
     (req, res) => {
